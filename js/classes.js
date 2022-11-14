4602505f3d7f42a115d25e6c05f8095954a9d651
Erwin Macaraig <erwin@kodego.ph>
2022-11-14 10:54:59 +0800

    first initial commit for demo

---
+++ js/classes.js
@@ -0,0 +1,63 @@
+class Calculator {
+  constructor() {       
+    this.result = 0;
+    this.calMem = [];
+    this.operations = [];    
+    this.input = 0;
+  }
+  add(firstnum, secondnum) {
+     return firstnum + secondnum;
+  }
+  
+  subtract(firstnum, secondnum) {    
+    return firstnum - secondnum;
+  }
+
+  getOperationResults() {
+    return this.result;
+  }
+  
+  displayResults() {
+    this.calMem.push(parseInt(this.input));
+     this.result = 0;
+    for (let i = 0; i < this.calMem.length - 1; i++) { 
+      if (this.operations[i]) { 
+        switch (this.operations[i]) { 
+          case '+':
+            this.calMem[i + 1] = this.add(this.calMem[i], this.calMem[i + 1]);
+            
+            break;
+          case '-':
+            this.calMem[i + 1] = this.subtract(this.calMem[i], this.calMem[i + 1]);
+            break;
+        }        
+      }
+     }
+    
+    document.getElementById('screen').value = this.calMem[this.calMem.length - 1];
+    this.result = this.calMem[this.calMem.length - 1];
+    this.calMem = [];
+    this.operations = [];  
+    this.input = '';
+  }
+
+  getInput() {
+    this.input = parseInt(document.getElementById('screen').value);
+  }
+  
+  setCalculatorOperation(op) {
+    this.calMem.push(parseInt(this.input));
+    this.operations.push(op);
+    document.getElementById('screen').value = '';    
+    document.getElementById('screen').focus();
+  }
+
+  cls() { 
+    document.getElementById('screen').value = '';
+    this.calMem = [];
+    this.operations = [];  
+    this.input = '';
+    this.result = 0;
+  }
+
+}
---
+++ js/index.js
@@ -0,0 +1,31 @@
+document.querySelector("div.container").style = "display: none";
+
+setTimeout(function () { 
+    document.querySelector("div.page-loading-status").style = "display: none";
+    
+    document.querySelector("div.container").style = "display: block";
+}, 2000);
+
+
+function startMyTime() { 
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    
+    // prepend 0
+    if (m < 10) { 
+        m = "0" + m;
+    }
+    if (s < 10) { 
+        s = "0" + s
+    }
+    document.getElementById("display-time").innerHTML = `${h}:${m}:${s}`;
+    t = setTimeout('startMyTime()', 500);
+
+
+}
+let calculator = new Calculator();
+
+
+
