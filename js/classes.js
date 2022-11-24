5bf678895de02f27095a0f88b7c74fc5b0950d4d
hellojhonas <jhonacenas0704@gmail.com>
2022-11-24 18:31:04 +0800

    bind key not yet finished

--- js/classes.js
+++ js/classes.js
@@ -1,24 +1,23 @@
 class Calculator {
-  constructor() {
+  constructor() {       
     this.result = 0;
     this.calMem = [];
-    this.operations = [];
+    this.operations = [];    
     this.input = 0;
   }
-  
   add(firstnum, secondnum) {
     return firstnum + secondnum;
   }
-
-  subtract(firstnum, secondnum) {
+  
+  subtract(firstnum, secondnum) {    
     return firstnum - secondnum;
   }
 
-  multiply(firstnum, secondnum) {
+  multiply(firstnum, secondnum){
     return firstnum * secondnum;
   }
 
-  divide(firstnum, secondnum) {
+  divide(firstnum, secondnum){
     return firstnum / secondnum;
   }
 
@@ -27,46 +26,92 @@ class Calculator {
   }
 
   displayResults() {
-    this.calMem.push(parseInt(this.input));
-    this.result = 0;
-    for (let i = 0; i < this.calMem.length - 1; i++) {
-      if (this.operations[i]) {
-        switch (this.operations[i]) {
+    this.calMem.push(parseFloat(this.input));
+     this.result = 0;
+    for (let i = 0; i < this.calMem.length - 1; i++) { 
+      if (this.operations[i]) { 
+        switch (this.operations[i]) { 
           case '+':
             this.calMem[i + 1] = this.add(this.calMem[i], this.calMem[i + 1]);
-
             break;
           case '-':
             this.calMem[i + 1] = this.subtract(this.calMem[i], this.calMem[i + 1]);
             break;
+          case '*':
+            this.calMem[i + 1] = this.multiply(this.calMem[i], this.calMem[i +1]);
+            break;
+          case '/':
+          this.calMem[i + 1] = this.divide(this.calMem[i], this.calMem[i +1]);
+          break;
         }
       }
-    }
+     }
 
     document.getElementById('screen').value = this.calMem[this.calMem.length - 1];
     this.result = this.calMem[this.calMem.length - 1];
     this.calMem = [];
-    this.operations = [];
+    this.operations = [];  
     this.input = '';
   }
 
-  getInput() {
-    this.input = parseInt(document.getElementById('screen').value);
+getInput(num) {
+  if(num){
+    if(num == 0){
+      this.input = parseFloat(document.getElementById('screen').value += String(num));
+    }
+    else{
+      this.input = parseFloat(document.getElementById('screen').value += num);
+    }
+   }
+   else{
+    this.input = parseFloat(document.getElementById('screen').value);
+   }
+
+}
+
+setCalculatorOperation(op) {
+  this.calMem.push(parseFloat(this.input));
+  this.operations.push(op);
+  document.getElementById('screen').value = '';
+}
+
+cls() { 
+  document.getElementById('screen').value = '';
+  this.calMem = [];
+  this.operations = [];  
+  this.input = '';
+  this.result = 0;
+}
+}
+class formatHours{
+  constructor(_hours){
+    this.Hrs = _hours;
+    this.ampm = _hours;
   }
 
-  setCalculatorOperation(op) {
-    this.calMem.push(parseInt(this.input));
-    this.operations.push(op);
-    document.getElementById('screen').value = '';
-    document.getElementById('screen').focus();
+  amPm(){
+    if(this.ampm > 12){
+      return this.ampm = "P.M";
+    }else if(this.ampm < 12 ){
+      return this.ampm = `A.M`;
+    }
   }
 
-  cls() {
-    document.getElementById('screen').value = '';
-    this.calMem = [];
-    this.operations = [];
-    this.input = '';
-    this.result = 0;
+  format12(){
+    if(this.Hrs >= 12){
+      if(this.Hrs == 12){
+        return this.Hrs;
+      }
+      else{
+        return this.Hrs = this.Hrs - 12;
+      }
+    }else{
+      if(this.Hrs == 0){
+        return this.Hrs + 12;
+      }else{
+        return this.Hrs;
+      }
+    }
   }
 
 }
--- js/index.js
+++ js/index.js
@@ -12,18 +12,61 @@ function startMyTime() {
     let h = today.getHours();
     let m = today.getMinutes();
     let s = today.getSeconds();
-    
-    // prepend 0
+
     if (m < 10) { 
         m = "0" + m;
     }
     if (s < 10) { 
         s = "0" + s;
     }
-    document.getElementById("display-time").innerHTML = `${h}:${m}:${s}`;
-    t = setTimeout('startMyTime()', 500);
+
+
+    let regular = new formatHours(h, m, s);
+    let chosenFormat = document.getElementById('hourFormat').value;
+    let regularHrs = regular.format12();
+    let formatOfTime = regular.amPm();
+    if(chosenFormat == "regular"){
+        document.getElementById("display-time").innerHTML = `${regularHrs}:${m}:${s}`;
+        document.getElementById('ampm').innerHTML = `${formatOfTime}`;
+        t = setTimeout('startMyTime()', 500);
+    }
+    else{
+        document.getElementById("display-time").innerHTML = `${h}:${m}:${s}`;
+        document.getElementById('ampm').innerHTML = ``;
+        t = setTimeout('startMyTime()', 500);
+    }
 }
+
 let calculator = new Calculator();
 
+document.getElementById('screen').onkeydown = (e) => calculator.bindKey(e)
 
-
+function keypress(event){
+    let operation = event.key;
+    document.getElementById('screen').focus();
+        switch (operation){
+            case '+':
+                document.getElementsByTagName('button')[0].focus();
+                document.getElementsByTagName('button')[0].click();
+                break;
+            case '-':
+                document.getElementsByTagName('button')[1].focus();
+                document.getElementsByTagName('button')[1].click();
+                break;
+            case '*':
+                document.getElementsByTagName('button')[2].focus();
+                document.getElementsByTagName('button')[2].click();
+                break;
+            case '/':
+                document.getElementsByTagName('button')[3].focus();
+                document.getElementsByTagName('button')[3].click();
+                break;
+            case 'Enter':
+                document.getElementsByTagName('button')[5].click();
+                break;
+            case 'c':
+                document.getElementsByTagName('button')[4].focus();
+                document.getElementsByTagName('button')[4].click();
+                break
+        }
+}
