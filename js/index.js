663f2019981c1164a687472e9dde865f884c767b
Glenzone <glennsonmadeja@gmail.com>
2022-12-01 00:00:28 +0800

    added 12-hr/24-hr switch | key binding still buggy

--- js/index.js
+++ js/index.js
@@ -25,5 +25,45 @@ function startMyTime() {
 }
 let calculator = new Calculator();
 
+document.getElementById("time-format").innerHTML = `24-Hour Format`;
+
+function clockFormat() {
+    format = document.getElementById("format-switch").checked;
+    document.getElementById("time-format").innerHTML = "";
+    if (format) {
+        document.getElementById("time-format").innerHTML = `12-Hour Format`;
+        clearTimeout(t);
+        document.getElementById("display-time").innerHTML = "";
+        const today = new Date();
+        let h = today.getHours();
+        let m = today.getMinutes();
+        let s = today.getSeconds();
+        if (h > 12) {
+            h12 = h - 12;
+            amPm = `PM`;
+        } else if (h == 0) {
+            h12 = 12;
+            amPm =`AM`;
+        } else {
+            h12 = h;
+            amPm `AM`;
+        }
+        // prepend 0
+        if (m < 10) { 
+            m = "0" + m;
+        }
+        if (s < 10) { 
+            s = "0" + s;
+        }
+        document.getElementById("display-time").innerHTML = `${h12}:${m}:${s} ${amPm}`;
+        t = setTimeout('clockFormat()', 500);
+
+    } else {
+        document.getElementById("time-format").innerHTML = `24-Hour Format`;
+        clearTimeout(t);
+        startMyTime();
+    };
+}
+
 
 
